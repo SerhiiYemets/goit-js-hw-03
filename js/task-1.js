d43c14012f2ae1d7986552ccfff37dfb7ccbb05e
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-08-17 18:40:31 +0200

    add js file

---
+++ js/task-1.js
@@ -0,0 +1,26 @@
+
+// перший спосiб 
+function slugify(title) {
+    let lower = title.toLowerCase();
+    let words = lower.split(" ");
+    let slug = words.join("-");
+    return slug;
+}
+
+
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// другий спосiб
+// function slugify(title) {
+//     return title.toLowerCase().split(" ").join("-");
+// }
+
+
+
+
+
+
